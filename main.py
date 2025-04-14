3308258ce78a87db1ec3ab8187d021294f4be2a6
morhaham <morhaham@Mors-MacBook-Pro.local>
2025-04-14 21:14:32 +0300

    exit gracefully. handle keyboard interruption

--- main.py
+++ main.py
@@ -15,9 +15,15 @@ def create_task_draft(title, description):
 def main_loop():
     click.echo("Welcome to Task Manager.")
     while True:
-        menu_choice = click.prompt(menu_controller.display_menu())
-        print(f"Chosen: {menu_choice}")
-        menu_controller.handle_choice(int(menu_choice) - 1)
+        try:
+            menu_choice = click.prompt(menu_controller.display_menu())
+            print(f"Chosen: {menu_choice}")
+            menu_controller.handle_choice(int(menu_choice) - 1)
+        # except CancelFlow:
+        #     click.echo("Returning to main menu")
+        except click.Abort:
+            click.echo("\nExiting gracefully, Bye!")
+            break
 
 if __name__ == '__main__':
     main_loop()
--- task_service.py
+++ task_service.py
@@ -3,6 +3,4 @@ from task import TaskContext, TaskDraft
 class TaskService:
     def create_task(self, title: str, description: str):
         return TaskContext(TaskDraft(title, description))
-
-    def list_tasks(self):
         
